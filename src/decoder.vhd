8ff1418a8a83a5a9f52a18155549ddcf6230596c
Bryan Herdianto <bryan.herdianto17@gmail.com>
2024-12-07 23:51:57 +0700

    semua kernel ada di folder kernels, semua output bmp di folder outputs, semua kode dan gambar input di folder source

---
+++ src/decoder.vhd
@@ -0,0 +1,28 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY DECODER IS
+    PORT (
+        CLK             : IN STD_LOGIC;
+        ENABLE          : IN STD_LOGIC;
+        INSTRUCTION     : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        OPCODE          : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+        ADDRESS_FLAG    : OUT STD_LOGIC
+    );
+END ENTITY DECODER;
+
+ARCHITECTURE RTL OF DECODER IS
+BEGIN
+    PROCESS (CLK)
+    BEGIN
+        IF RISING_EDGE(CLK) THEN
+            IF ENABLE = '1' THEN
+                OPCODE          <= INSTRUCTION(2 DOWNTO 0);
+                ADDRESS_FLAG    <= INSTRUCTION(3);
+            ELSE
+                OPCODE          <= (OTHERS => '0');
+                ADDRESS_FLAG    <= '0';
+            END IF;
+        END IF;
+    END PROCESS;
+END RTL;
